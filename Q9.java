3843c31f520219714cfd12d630040c8750a43594
shubhamRawat14 <94029087+shubhamRawat14@users.noreply.github.com>
2022-02-11 23:10:26 +0530

    Java Advance

--- Q9.java
+++ Q9.java
@@ -1,3 +1,10 @@
+/*
+Q9. Create a class TwoDim which contains private members as x and y coordinates in package P1. Define the default constructor, 
+a parameterized constructor and override toString() method to display the coordinates. Now reuse this class and in package P2 
+create another class ThreeDim, adding a new dimension as z as its private member. Define the constructors for the subclass and 
+override toString() method in the subclass also. Write appropriate methods to show dynamic method dispatch. The main() function 
+should be in a package P.
+*/
 class Main {
     public static void main(String[] args) {
         TwoDim reference;           // obtain a reference of type TwoDim
